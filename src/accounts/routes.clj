f01498a65a244938b020c7c95569484b28b3bff0
Rafael Leal <rafaelealdias@gmail.com>
2018-03-28 13:30:12 -0300

    Connecting routes

--- src/accounts/routes.clj
+++ src/accounts/routes.clj
@@ -5,12 +5,39 @@
             [common-labsoft.pedestal.interceptors.schema :as int-schema]
             [io.pedestal.http.route.definition :refer [defroutes]]
             [io.pedestal.http.body-params :as body-params]
-            [schema.core :as s]))
+            [accounts.models.carrier :as models.carrier]
+            [accounts.models.customer :as models.customer]
+            [accounts.db.datomic.carrier :as datomic.carrier]
+            [accounts.controllers.customer :as controllers.customer]
+            [accounts.controllers.carrier :as controllers.carrier]
+            [schema.core :as s]
+            [accounts.db.datomic.customer :as datomic.customer]))
 
 (defn hello-world
   [request]
   {:status 200
    :body   {:res "Hello, World!"}})
+
+(defn get-one-customer-route
+  [{{:keys [crypto token datomic http sqs]} :components customer-id :customer-id}]
+  {:status 200
+   :body   (controllers.customer/get-customer! customer-id datomic)})
+
+(defn create-customer-route
+  [{{:keys [crypto token datomic http sqs]} :components customer :data}]
+  {:status 200
+   :body   (controllers.customer/new-customer-account! customer datomic)})
+
+(defn get-one-carrier-route
+  [{{:keys [crypto token datomic http sqs]} :components carrier-id :carrier-id}]
+  {:status 200
+   :body   (controllers.carrier/get-carrier! carrier-id datomic)})
+
+(defn create-carrier-route
+  [{{:keys [crypto token datomic http sqs]} :components carrier :data}]
+  {:status 200
+   :body   (controllers.carrier/new-carrier-account! carrier datomic)})
+
 
 (defroutes routes
   [[["/" ^:interceptors [int-err/catch!
@@ -21,28 +48,20 @@
                          int-schema/coerce-output]
      ["/api"
 
-      ["/users/:id" ^:interceptors [(int-adapt/path->uuid :id :customer-id)]
-
-       ["/customer" ^:interceptors [(int-auth/scopes-or? #{"auth"} (partial int-auth/auth-owner? :id '[[?entity :customer/user-id ?resource-id]
-                                                                                                       [?entity :customer/id ?owner-id]]))]
-        {:get [:customer-by-user-id hello-world]}]
-
-       ["/carrier" ^:interceptors [(int-auth/scopes-or? #{"auth"} (partial int-auth/auth-owner? :id '[[?entity :carrier/user-id ?resource-id]
-                                                                                                      [?entity :carrier/id ?owner-id]]))]
-        {:get [:carrier-by-user-id hello-world]}]]
-
       ["/customers"
-       {:post [:new-customer ^:interceptors [(int-auth/allow-scopes? "auth")]
-               hello-world]}
+       {:post [:new-customer ^:interceptors [(int-auth/allow-scopes? "auth")
+                                             (int-schema/coerce models.customer/Customer)]
+               create-customer-route]}
 
        ["/:id" ^:interceptors [(int-adapt/path->uuid :id :customer-id)
                                (int-auth/scopes-or? #{"auth"} (partial int-auth/auth-identity? :id))]
-        {:get [:one-customer hello-world]}]]
+        {:get [:one-customer get-one-customer-route]}]]
 
       ["/carriers"
-       {:post [:new-carrier ^:interceptors [(int-auth/allow-scopes? "auth")]
-               hello-world]}
+       {:post [:new-carrier ^:interceptors [(int-auth/allow-scopes? "auth")
+                                            (int-schema/coerce models.carrier/Carrier)]
+               create-carrier-route]}
 
        ["/:id" ^:interceptors [(int-adapt/path->uuid :id :carrier-id)
                                (int-auth/scopes-or? #{"auth"} (partial int-auth/auth-identity? :id))]
-        {:get [:one-carrier hello-world]}]]]]]])
+        {:get [:one-carrier get-one-carrier-route]}]]]]]])
--- test/accounts/service_test.clj
+++ test/accounts/service_test.clj
@@ -1,4 +1,4 @@
-(ns accounts.service-test
+  (ns accounts.service-test
   (:require [midje.sweet :refer :all]
             [accounts.service :as ser]
             [common-labsoft.test-helpers :as th]))
@@ -6,4 +6,4 @@
 (th/with-service [ser/start! ser/stop!] [system service]
   (th/with-token {:token/scopes #{"customer"} :token/sub "3B684A44-3994-49A3-8112-29C4651D78BD"}
     (fact "Http Test"
-      (th/request! service :get "/api/customers/3B684A44-3994-49A3-8112-29C4651D78BD") => {:res "Hello, World!"})))
+      (th/request! service :get "/api/customers/3B684A44-3994-49A3-8112-29C4651D78BD") => {:error "NotFound"})))
